a977b79f8d5b15ab5b79ac20abaf042130e87ac0
Leooo 3 <leokeene@icloud.com>
2023-12-22 11:25:21 +0000

    bun.serve

--- get_auth.js
+++ get_auth.js
@@ -1,5 +1,19 @@
 const express = require('express');
-var app = express();
 // remember to set environment variables 
 var CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 var REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+var scope = "playlist-read-private playlist-modify-private playlist-modify-public";
+
+Bun.serve({
+    fetch(req) {
+        var state = Math.random().toString(36).substring(2,18)
+        const url = new URL(req.url);
+        if (url.pathname == "/login") return new Response(JSON.stringify({
+            response_type: 'code',
+            client_id: CLIENT_ID,
+            scope: scope,
+            redirect_uri: REDIRECT_URI,
+            state: state
+        }));
+    },
+});
