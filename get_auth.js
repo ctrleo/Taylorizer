12d38998eb6e07569e829442cfb54ae87c0d64c0
Leooo 3 <120170440+ctrleo@users.noreply.github.com>
2023-12-22 14:23:38 +0000

    Update get_auth.js
    I'm sorry

--- get_auth.js
+++ get_auth.js
@@ -6,14 +6,24 @@ var scope = "playlist-read-private playlist-modify-private playlist-modify-publi
 
 Bun.serve({
     fetch(req) {
-        var state = Math.random().toString(36).substring(2,18)
         const url = new URL(req.url);
-        if (url.pathname == "/login") return new Response('https://accounts.spotify.com/authorize?' + queryString.stringify({
+        if (url.pathname == "/login") {
+            var state = Math.random().toString(36).substring(2,18);
+            return new Response('https://accounts.spotify.com/authorize?' + queryString.stringify({
             response_type: 'code',
             client_id: CLIENT_ID,
             scope: scope,
             redirect_uri: REDIRECT_URI,
             state: state
-        }));
-    },
+            }));
+        if (url.pathname == "/callback") {
+            var code = req.query.code || null;
+            var state = req.query.state || null;
+            if (state === null) {
+                console.log("Attempted spotify login failed! :( (state mismatch occurred)");
+                res.redirect("https://ctrleo.github.com/taylorizer/#?error="state");
+            }
+            };
+        };
+    };
 });
