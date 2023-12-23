9fad203bdec468396357c88c4f446e87e2c35ee4
Leo K <leokeene@icloud.com>
2023-12-23 15:47:36 +0000

    turned Response.redirect into normal response because ngrok is mean

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ Bun.serve({
         if (url.pathname == "/login") {
             console.log("Spotify login requested")
             var state = Math.random().toString(36).substring(2,18);
-            return Response.redirect('https://accounts.spotify.com/authorize?' + queryString.stringify({
+            return Response('https://accounts.spotify.com/authorize?' + queryString.stringify({
                 response_type: 'code',
                 client_id: CLIENT_ID,
                 redirect_uri: REDIRECT_URI,
@@ -27,7 +27,7 @@ Bun.serve({
             var code = req.query.code || null;
             if (state == null) {
                 console.log("ATTEMPTED LOGIN FAILED!!! (state mismatch err)");
-                return Response.redirect("https://ctrleo.github.io/taylorizer/#?err=state-mismatch")
+                return Response("https://ctrleo.github.io/taylorizer/#?err=state-mismatch")
             } else {
                 var access = fetch("https://accounts.spotify.com", {
                     method: "POST",
